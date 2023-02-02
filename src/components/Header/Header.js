3e4ef5d4505e94e00074352eec03f65afc1d7e07
Jasmine Hendricks <jasmine@justappraised.com>
2023-02-01 20:57:59 -0700

    added react icons, init setup of header complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { BsHeart } from "react-icons/bs";
+import { MdOutlineMail } from "react-icons/md";
+// import home from '../../assets/icon-home.png'
 import './Header.css'
 
 const Header = () => {
   return (
-    <div>Header </div>
+    <div className='header'>
+      <div className='home-button'>
+        🏠
+        Unilife
+      </div>
+      <div className='right-side'>
+        <div className='shortlist'>
+          <BsHeart />
+          Shortlist
+        </div>
+        <div className='contact-us'>
+          <MdOutlineMail />
+          Contact Us
+        </div>
+      </div>
+    </div>
   )
 }
 
