411840bf1964ab491049d04350567314d440b6d9
Jasmine Hendricks <jasmine@justappraised.com>
2023-02-01 21:30:49 -0700

    initial search bar set up complete

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Form } from 'react-router-dom'
 import './SearchBox.css'
 
 const SearchBox = () => {
   return (
-    <div>Search Box</div>
+    <div>
+      <form className='search-box-container'>
+        <select>
+          <option selected value="placeholder">Search by city</option>
+          <option value="placeholder">1</option>
+          <option value="placeholder">2</option>
+          <option value="placeholder">3</option>
+        </select>
+        <select>
+          <option selected value="placeholder">Any Bedroom</option>
+          <option value="placeholder">1</option>
+          <option value="placeholder">2</option>
+          <option value="mango">3</option>
+        </select>
+        <input type="submit" value="Find Homes" />
+      </form>
+    </div>
   )
 }
 
