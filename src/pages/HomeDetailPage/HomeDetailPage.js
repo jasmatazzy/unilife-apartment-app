e7752b4fc25af18ec453835916c058e03255638f
Jasmine Hendricks <jasmine@justappraised.com>
2023-02-01 20:26:19 -0700

    textual set up of home details page complete

--- src/pages/HomeDetailPage/HomeDetailPage.js
+++ src/pages/HomeDetailPage/HomeDetailPage.js
@@ -4,7 +4,24 @@ import './HomeDetailPage.css'
 const HomeDetailPage = () => {
   return (
     <div>
-      
+      <div className='pics-and-basics-container'>
+        <div className='pics-container'>"I contain pics"</div>
+        <div className='basics-container'>" I contain address and logistics"</div>
+      </div>
+      <div className='description-and-bedroom-pricing-container'>
+        <div className='description'>
+          <h2>description</h2>
+          <p>"I contain the apartment description"</p>
+        </div>
+        <div className='basics-container'>
+          <h2>Bedroom Prices</h2>
+          <p>"I contain info on price per bedroom"</p>
+        </div>
+      </div>
+      <div className='key-features-container'>
+        <h2>Key Features</h2>
+        <p>"I contain info on key features"</p>
+      </div>
     </div>
   )
 }
