73b34cf84b16f4009eeb5f296a4e9c9169741fab
Jasmine Hendricks <jasmine@justappraised.com>
2023-02-03 15:56:45 -0700

    basic formatting of footer complete

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import './Footer.css'
+import { BsFacebook } from "react-icons/bs";
+import {AiFillFacebook, AiFillTwitterCircle, AiFillInstagram } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <div className='footer-container'>
-      <div className='blue-div'>blue</div>
+      <div className='blue-div'>
+        <div>
+          <h2>Keep in touch</h2>
+          <p>Curious about new offerings? Sign up for our weekly newsletter and stay informed.</p>
+          <input type="email" placeholder='Your email' name='userEmail'/>
+        </div>
+        <div>
+        <h2> Let's Socialize</h2>
+        <p><AiFillFacebook /> Facebook</p>
+        <p><AiFillTwitterCircle/> Twitter</p>
+        <p><AiFillInstagram/>Instagram</p>
+        </div>
+      </div>
       <div className='black-div'>
         <div>
           <p>About Us</p>
