6edde75cfc6f1666d0e852085fc5162105d58420
Jasmine Hendricks <jasmine@justappraised.com>
2023-02-12 02:08:56 -0700

    core components of project is complete, including final page that needed to be  added in for individual  homes. still need to  work on search box and modals, in addition to a multitude of css optimizations.

--- src/pages/HomeDetailPage/HomeDetailPage.js
+++ src/pages/HomeDetailPage/HomeDetailPage.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import bedroomIcon from "../../assets/icon-bedroom-blue.png";
+import bathroomIcon from "../../assets/icon-bathroom-blue.png";
 import useFetch from "../../utils/useFetch";
 import "./HomeDetailPage.css";
 
@@ -12,15 +14,93 @@ const HomeDetailPage = () => {
     isLoaded,
     error,
     data: homeDetails,
-  } = useFetch("https://unilife-server.herokuapp.com/properties/6364c5fdfff841b8724bacc9");
-console.log(JSON.stringify(homeDetails, null, 2))
+  } = useFetch(`https://unilife-server.herokuapp.com/properties/${homeID}`);
+  // console.log(JSON.stringify(homeDetails?.address, null, 2));
+  
+  console.log(homeDetails);
 
 
-  //from apartment, the component will get: mainImage, otherImages, address, bedrooms,
-  // bathrooms, home type, price, furnishedType, availability, description, keyFeaturesArray,
-  // bedroomPricesArray
 
-  return <div> some stufff</div>;
+  return (
+    <div className="home-detail-page-container">
+      <div className="main-div">
+        <div className="home-detail-pictures">
+          <img
+            src={homeDetails?.images?.[0]}
+            alt="main"
+            id="home-detail-main-picture"
+          />
+          <div className="secondary-pictures-container">
+            {homeDetails.images?.slice(1, 9).map((photoURL) => (
+              <img src={photoURL} alt="add'l visual" />
+            ))}
+          </div>
+        </div>
+        <div className="address-and-buttons-container">
+          <div className="address-info">
+            <h1>
+              {homeDetails.address?.street} {homeDetails.address?.city}{" "}
+              {homeDetails.address?.postcode}
+            </h1>
+            <div className="home-detail-card-all">
+              <div className="home-detail-card-div-top">
+                <div>
+                  <p>Bedrooms</p>
+                  <img src={bedroomIcon} alt="bedroom" />
+                  <span>{homeDetails?.bedroom_count}</span>
+                </div>
+                <div>
+                  <p>Bathrooms</p>
+                  <img src={bathroomIcon} alt="bath" />
+                  <span>{homeDetails?.bathroom_count}</span>
+                </div>
+                <div>
+                  <p>Property Type</p>
+                  <span>{homeDetails?.property_type}</span>
+                </div>
+              </div>
+              <div className="home-detail-card-div-bottom">
+                <div>
+                  <p>Price</p>
+                  <span>£{homeDetails?.rent}</span>
+                </div>
+                <div>
+                  <p>Furnished Type</p>
+                  <span>{homeDetails?.furnished}</span>
+                </div>
+                <div>
+                  <p>Available from</p>
+                  <span>{homeDetails?.availability}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="address-buttons-container">
+            <button className="shortlist-button">Shortlist</button>
+            <button className="book-viewing-button">Book Viewing </button>
+          </div>
+        </div>
+      </div>
+      <div className="description-and-bedroom-prices-container">
+        <div className="home-detail-description-container">
+          <h2>Description</h2>
+          <p>{homeDetails?.property_description}</p>
+        </div>
+        <div>
+          <h2>Bedroom Prices</h2>
+          <div className="home-detail-bedroom-prices">
+            bedroom prices go here
+          </div>
+        </div>
+      </div>
+      <div className="key-features">
+        <h2>Key Features</h2>
+        {homeDetails.key_features?.map(
+          (feature)=> <p>✔️{feature}</p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default HomeDetailPage;
